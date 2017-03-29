21d4611483dd43e22bf6c2eabe136d089cb6c7c1
Hari Prasath S <hariprasathhari9292@gmail.com>
2017-03-29 15:52:13 +0530

    settings modified

--- proj/settings.py
+++ proj/settings.py
@@ -113,6 +113,18 @@ DATABASES = {
 
 
 
+DATABASES = {
+   'default': {
+      'ENGINE': 'django.db.backends.mysql',
+      'NAME': 'hackathon_db',
+      'USER': 'devil',
+      'PASSWORD': 'harihari',
+      'HOST': '117.239.246.58',
+      'PORT': '3306',
+   }
+}
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
