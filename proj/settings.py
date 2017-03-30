eab9d5d70af09ff1b38abeea48a3b7bb7241a77a
Hari Prasath S <hariprasathhari9292@gmail.com>
2017-03-30 11:14:54 +0530

    middleware added again

--- proj/settings.py
+++ proj/settings.py
@@ -50,7 +50,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'corsheaders.miidleware.CorsMiddleware'
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
