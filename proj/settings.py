55700882cd5155bafe77c5dbedf71bc353f09936
siddy7 <siddharthan.ss@gmail.com>
2017-04-01 09:02:29 +0530

    changed db

--- proj/settings.py
+++ proj/settings.py
@@ -104,17 +104,17 @@ DATABASES = {
 }
 
 
-#
-# DATABASES = {
-#    'default': {
-#       'ENGINE': 'django.db.backends.mysql',
-#       'NAME': 'hackathon_db',
-#       'USER': 'devil',
-#       'PASSWORD': 'harihari',
-#       'HOST': '117.239.246.58',
-#       'PORT': '3306',
-#    }
-# }
+
+DATABASES = {
+   'default': {
+      'ENGINE': 'django.db.backends.mysql',
+      'NAME': 'hackathon_db',
+      'USER': 'devil',
+      'PASSWORD': 'harihari',
+      'HOST': '117.239.246.58',
+      'PORT': '3306',
+   }
+}
 
 
 
