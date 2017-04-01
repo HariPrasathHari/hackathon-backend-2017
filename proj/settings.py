f3dad173f9b3a13b574a2a00d9f3117b9a79c877
siddy7 <siddharthan.ss@gmail.com>
2017-04-02 03:14:48 +0530

    added datacenter app

--- proj/settings.py
+++ proj/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'profiledet',
     'ApplyScheme',
     'Status',
+    'datacenter',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
